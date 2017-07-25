2a142795d64497e1bb2463ddd594bc0be65e0467
BunChan <bunchan16@yahoo.com>
2017-07-25 23:43:28 +0800

    add webviewjavascriptbridge

---
+++ NewsDetailDemo/NewsDetailDemo/Other/Bridge.h
@@ -0,0 +1,14 @@
+//
+//  Bridge.h
+//  NewsDetailDemo
+//
+//  Created by Bin on 2017/7/25.
+//  Copyright © 2017年 Urun. All rights reserved.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+#import "WebViewJavascriptBridge.h"
+
+#endif /* Bridge_h */
