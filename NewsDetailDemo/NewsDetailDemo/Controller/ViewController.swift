19268d1fee7a7bdb1d4bc2aa115648f64eee1286
BunChan <bunchan16@yahoo.com>
2017-08-06 17:19:15 +0800

    展示content

--- NewsDetailDemo/NewsDetailDemo/Controller/ViewController.swift
+++ NewsDetailDemo/NewsDetailDemo/Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
@@ -14,14 +15,63 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let model = NewsDetail.init()
-        print(model.imageArr[0].originalPath)
+        let width = UIScreen.main.bounds.width
+        var tempHtml = try? String(contentsOf: Bundle.main.url(forResource: "ArticleDetail", withExtension: "html")!, encoding: String.Encoding.utf8)
+        
+        var bodyStr = "<body style=\"font-size:\(25)px;color:\("#4f4f4f");letter-spacing:0px;word-spacing:2px;text-align:justify;overflow:hidden;margin:10px 15px;width=\(width)px;word-break:break-all\">"
+        
+        bodyStr += "<p><div class='content'>\(model.content)</div></p>"
+        bodyStr += "</body>"
+        
+        tempHtml = tempHtml?.replacingOccurrences(of: "<body><mainView></body>", with: bodyStr)
+        
+        self.view.addSubview(newsDetailWebView)
+        newsDetailWebView.loadHTMLString(tempHtml!, baseURL: Bundle.main.bundleURL)
+        
+        let h = newsDetailWebView.stringByEvaluatingJavaScript(from: "document.body.scrollHeight")
+        
+//        let height = Int(h!)
+        let height = Float(h!)
+        mainScrollView.contentSize = CGSize(width: 0, height: Int(height!))
+        
+        self.view.addSubview(mainScrollView)
+        mainScrollView.snp.makeConstraints {
+            $0.bottom.left.right.equalTo(self.view)
+            $0.top.equalTo(self.view).offset(20)
+        }
+
+        mainScrollView.addSubview(newsDetailWebView)
+        newsDetailWebView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 667)
+//        newsDetailWebView.snp.makeConstraints {
+//            $0.left.right.top.equalTo(self.view)
+//            $0.height.equalTo(height!)
+//        }
+        
+//        self.view.addSubview(newsDetailWebView)
+//        newsDetailWebView.snp.makeConstraints {
+//            $0.edges.equalTo(self.view)
+//        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    fileprivate lazy var newsDetailWebView: UIWebView = {
+        let webView = UIWebView()
+        return webView
+    }()
 
-
+    fileprivate lazy var mainScrollView: UIScrollView = {
+        let tempScrollView = UIScrollView()
+        tempScrollView.backgroundColor = .red
+        tempScrollView.showsVerticalScrollIndicator = true
+        tempScrollView.showsHorizontalScrollIndicator = false
+//        tempScrollView.contentSize = CGSize(width: 1500, height: 1500)
+        return tempScrollView
+    }()
+    
 }
 
