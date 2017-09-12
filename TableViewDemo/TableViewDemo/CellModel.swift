e6895f248212afb472834c185b155b649f064e30
BunChan <bunchan16@yahoo.com>
2017-09-12 14:44:08 +0800

    创建demo

---
+++ TableViewDemo/TableViewDemo/CellModel.swift
@@ -0,0 +1,22 @@
+//
+//  CellModel.swift
+//  TableViewDemo
+//
+//  Created by Bin on 2017/9/12.
+//  Copyright © 2017年 Urun. All rights reserved.
+//
+
+import Foundation
+
+
+struct cellModel {
+    
+    var title: String = ""
+    var id: Int = 0
+    
+    init(title: String, id: Int) {
+        self.title = title
+        self.id = id
+    }
+    
+}
