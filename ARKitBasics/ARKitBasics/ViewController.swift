6468a3d6bcef990f1e2c2ca2d742de3bacd69e34
BunChan <bunchan16@yahoo.com>
2017-12-08 19:48:46 +0800

    修复bug

--- ARKitBasics/ARKitBasics/ViewController.swift
+++ ARKitBasics/ARKitBasics/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
 
     // MARK: - IBOutlets
     
-
     @IBOutlet weak var sceneView: ARSCNView!
     
     override func viewDidAppear(_ animated: Bool) {
@@ -39,6 +38,8 @@ class ViewController: UIViewController {
         sceneView.session.delegate = self
         
         UIApplication.shared.isIdleTimerDisabled = true
+        
+        sceneView.showsStatistics = true
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -52,6 +53,46 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    fileprivate func updateSessionInfoLabel(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
+        // Update the UI to provide feedback on the state of the AR experience.
+        let message: String
+        
+        switch trackingState {
+        case .normal where frame.anchors.isEmpty:
+            // No planes detected; provide instructions for this app's AR interactions.
+            message = "Move the device around to detect horizontal surfaces."
+            
+        case .normal:
+            // No feedback needed when tracking is normal and planes are visible.
+            message = ""
+            
+        case .notAvailable:
+            message = "Tracking unavailable."
+            
+        case .limited(.excessiveMotion):
+            message = "Tracking limited - Move the device more slowly."
+            
+        case .limited(.insufficientFeatures):
+            message = "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
+            
+        case .limited(.initializing):
+            message = "Initializing AR session."
+            
+        }
+        
+        print(message)
+        
+//        sessionInfoLabel.text = message
+//        sessionInfoView.isHidden = message.isEmpty
+    }
+    
+    fileprivate func restTracking() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -62,6 +103,8 @@ class ViewController: UIViewController {
 
 
 extension ViewController: ARSCNViewDelegate, ARSessionDelegate {
+    
+    //MARK: - ARSCNViewDelegate
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
@@ -88,6 +131,32 @@ extension ViewController: ARSCNViewDelegate, ARSessionDelegate {
         
         plane.width = CGFloat(planeAnchor.extent.x)
         plane.height = CGFloat(planeAnchor.extent.z)
+    }
+    
+    //MARK: - ARSessionDelegate
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        updateSessionInfoLabel(for: session.currentFrame!, trackingState: camera.trackingState)
+    }
+    
+    //MARK: - ARSessionObserver
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        print("session中断")
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        restTracking()
     }
     
     
